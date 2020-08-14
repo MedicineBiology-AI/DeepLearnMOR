587471318ee9e3675032aa5d20c7ac7d4aa01058
sunshineAB <1571537695@qq.com>
2020-08-14 16:14:45 +0800

    update

--- code/nets/bottleneck.py
+++ code/nets/bottleneck.py
@@ -15,45 +15,48 @@ from nets import utils
 
 MAX_NUM_IMAGES_PER_CLASS = 2 ** 27 - 1
 
+
 def get_bottleneck_path(image_lists, label_name, index, bottleneck_dir, category, model_name):
-  return utils.get_image_path(image_lists, label_name, index, bottleneck_dir, category) +"_"+ model_name+".h5"
+    return utils.get_image_path(image_lists, label_name, index, bottleneck_dir, category) + "_" + model_name + ".h5"
+
 
 # Store bottleneck values
-def store_bottlenecks(sess, image_lists, image_dir, bottleneck_dir,model_name,
+def store_bottlenecks(sess, image_lists, image_dir, bottleneck_dir, model_name,
                       jpeg_data_tensor, decoded_image_tensor,
-                      resized_input_tensor, bottleneck_tensor,model=None):
-  num_bottlenecks = 0
+                      resized_input_tensor, bottleneck_tensor, model=None):
+    num_bottlenecks = 0
 
-  if not os.path.exists(bottleneck_dir):
-      os.makedirs(bottleneck_dir)
+    if not os.path.exists(bottleneck_dir):
+        os.makedirs(bottleneck_dir)
 
-  for label_name, label_lists in image_lists.items():
-    for category in ["training", "testing", "validation"]:
-      category_list = label_lists[category]
-      for index in range(len(category_list)):
-        get_bottleneck(
-          sess, image_lists, label_name, index, image_dir, category,
-          bottleneck_dir, model_name, jpeg_data_tensor, decoded_image_tensor,
-          resized_input_tensor, bottleneck_tensor,model=model)
+    for label_name, label_lists in image_lists.items():
+        for category in ["training", "testing", "validation"]:
+            category_list = label_lists[category]
+            for index in range(len(category_list)):
+                get_bottleneck(
+                    sess, image_lists, label_name, index, image_dir, category,
+                    bottleneck_dir, model_name, jpeg_data_tensor, decoded_image_tensor,
+                    resized_input_tensor, bottleneck_tensor, model=model)
 
-        num_bottlenecks += 1
-        if num_bottlenecks % 100 == 0:
-          tf.logging.info("{} bottleneck files created.".format(num_bottlenecks))
+                num_bottlenecks += 1
+                if num_bottlenecks % 100 == 0:
+                    tf.logging.info("{} bottleneck files created.".format(num_bottlenecks))
+
 
 # Calculate bottleneck values for image
 def get_bottleneck(sess, image_lists, label_name, index, image_dir,
                    category, bottleneck_dir, model_name, jpeg_data_tensor,
                    decoded_image_tensor, resized_input_tensor,
-                   bottleneck_tensor,model=None):
+                   bottleneck_tensor, model=None):
     label_lists = image_lists[label_name]
     bottleneck_path = get_bottleneck_path(image_lists, label_name, index,
-                                                bottleneck_dir, category,model_name)
+                                          bottleneck_dir, category, model_name)
     if not os.path.exists(bottleneck_path):
         create_bottleneck(
             sess, bottleneck_path, image_lists, label_name, index,
             image_dir, category, jpeg_data_tensor,
             decoded_image_tensor, resized_input_tensor,
-            bottleneck_tensor,model=model)
+            bottleneck_tensor, model=model)
 
     with h5py.File(bottleneck_path, "r") as bottleneck_file:
         bottleneck_string = np.array(bottleneck_file['bottleneck'])
@@ -67,7 +70,7 @@ def get_bottleneck(sess, image_lists, label_name, index, image_dir,
             sess, bottleneck_path, image_lists, label_name, index,
             image_dir, category, jpeg_data_tensor,
             decoded_image_tensor, resized_input_tensor,
-            bottleneck_tensor,model=model)
+            bottleneck_tensor, model=model)
 
         with h5py.File(bottleneck_path, "r") as bottleneck_file:
             bottleneck_string = np.array(bottleneck_file['bottleneck'])
@@ -75,11 +78,12 @@ def get_bottleneck(sess, image_lists, label_name, index, image_dir,
         bottleneck_values = bottleneck_string
     return bottleneck_values
 
+
 # Creates a bottleneck file
-def create_bottleneck(sess,bottleneck_path, image_lists, label_name, index,
+def create_bottleneck(sess, bottleneck_path, image_lists, label_name, index,
                       image_dir, category, jpeg_data_tensor,
                       decoded_image_tensor, resized_input_tensor,
-                      bottleneck_tensor,model=None):
+                      bottleneck_tensor, model=None):
     tf.logging.info("Creating Bottleneck at {}".format(bottleneck_path))
     image_path = utils.get_image_path(image_lists, label_name, index,
                                       image_dir, category)
@@ -87,7 +91,7 @@ def create_bottleneck(sess,bottleneck_path, image_lists, label_name, index,
         tf.logging.fatal("File does not exist {}".format(image_path))
     image_data = gfile.FastGFile(image_path, "rb").read()
     try:
-        if(model==None):
+        if (model == None):
             bottleneck_values = run_bottleneck(
                 sess, image_data, jpeg_data_tensor, decoded_image_tensor,
                 resized_input_tensor, bottleneck_tensor)
@@ -106,70 +110,73 @@ def create_bottleneck(sess,bottleneck_path, image_lists, label_name, index,
     with h5py.File(bottleneck_path, "w") as bottleneck_file:
         bottleneck_file['bottleneck'] = bottleneck_values
 
+
 # Run pre-train model on an image to generate bottleneck values
 def run_bottleneck(sess, image_data, image_data_tensor,
-                            decoded_image_tensor, resized_input_tensor,
-                            bottleneck_tensor):
-  # Preprocess input images
-  resized_input_values = sess.run(decoded_image_tensor,
-                                  {image_data_tensor: image_data})
-  # Run resize datas through the network.
-  bottleneck_values = sess.run(bottleneck_tensor,
-                               {resized_input_tensor: resized_input_values})
+                   decoded_image_tensor, resized_input_tensor,
+                   bottleneck_tensor):
+    # Preprocess input images
+    resized_input_values = sess.run(decoded_image_tensor,
+                                    {image_data_tensor: image_data})
+    # Run resize datas through the network.
+    bottleneck_values = sess.run(bottleneck_tensor,
+                                 {resized_input_tensor: resized_input_values})
 
-  bottleneck_values = np.squeeze(bottleneck_values)
-  return bottleneck_values
+    bottleneck_values = np.squeeze(bottleneck_values)
+    return bottleneck_values
+
 
 # Run densenet on an image to generate bottleneck values
 def run_densenet_bottleneck(sess, image_data, image_data_tensor,
                             decoded_image_tensor,
                             bottleneck_tensor, model):
-  # Preprocess input images
-  resized_input_values = sess.run(decoded_image_tensor,
+    # Preprocess input images
+    resized_input_values = sess.run(decoded_image_tensor,
                                     {image_data_tensor: image_data})
-  # Run resize datas through the network.
-  outputs = model.predict(resized_input_values)
+    # Run resize datas through the network.
+    outputs = model.predict(resized_input_values)
 
-  bottleneck_values = sess.run(bottleneck_tensor, {bottleneck_tensor: outputs})
-  bottleneck_values = np.squeeze(bottleneck_values)
+    bottleneck_values = sess.run(bottleneck_tensor, {bottleneck_tensor: outputs})
+    bottleneck_values = np.squeeze(bottleneck_values)
 
-  return bottleneck_values
+    return bottleneck_values
+
 
 # Returns a batch of the bottlenecks from storage
 def get_batch_of_bottlenecks(sess, image_lists, batch_size, category,
-                                  bottleneck_dir, image_dir, model_name, jpeg_data_tensor,
-                                  decoded_image_tensor, resized_input_tensor,
-                                  bottleneck_tensor):
-  class_count = len(image_lists.keys())
-  bottlenecks = []
-  labels = []
-  if batch_size >= 0:
-    # Fetch a random sample of bottlenecks.
-    # Used for training set.
-    for i in range(batch_size):
-      label_index = random.randrange(class_count)
-      label_name = list(image_lists.keys())[label_index]
-      image_index = random.randrange(MAX_NUM_IMAGES_PER_CLASS + 1)
-      bottleneck = get_bottleneck(
-        sess, image_lists, label_name, image_index, image_dir, category,
-        bottleneck_dir, model_name, jpeg_data_tensor, decoded_image_tensor,
-        resized_input_tensor, bottleneck_tensor)
-      ground_truth = np.zeros(class_count, dtype=np.float32)
-      ground_truth[label_index] = 1.0
-      bottlenecks.append(bottleneck)
-      labels.append(ground_truth)
-  else:
-    # Fetch all bottlenecks
-    # Used for validation set and test set.
-    for label_index, label_name in enumerate(image_lists.keys()):
-      for image_index, image_name in enumerate(
-          image_lists[label_name][category]):
-        bottleneck = get_bottleneck(
-          sess, image_lists, label_name, image_index, image_dir, category,
-          bottleneck_dir, model_name, jpeg_data_tensor, decoded_image_tensor,
-          resized_input_tensor, bottleneck_tensor)
-        ground_truth = np.zeros(class_count, dtype=np.float32)
-        ground_truth[label_index] = 1.0
-        bottlenecks.append(bottleneck)
-        labels.append(ground_truth)
-  return bottlenecks, labels
+                             bottleneck_dir, image_dir, model_name, jpeg_data_tensor,
+                             decoded_image_tensor, resized_input_tensor,
+                             bottleneck_tensor):
+    class_count = len(image_lists.keys())
+    bottlenecks = []
+    labels = []
+    if batch_size >= 0:
+        # Fetch a random sample of bottlenecks.
+        # Used for training set.
+        for i in range(batch_size):
+            label_index = random.randrange(class_count)
+            label_name = list(image_lists.keys())[label_index]
+            image_index = random.randrange(MAX_NUM_IMAGES_PER_CLASS + 1)
+            bottleneck = get_bottleneck(
+                sess, image_lists, label_name, image_index, image_dir, category,
+                bottleneck_dir, model_name, jpeg_data_tensor, decoded_image_tensor,
+                resized_input_tensor, bottleneck_tensor)
+            ground_truth = np.zeros(class_count, dtype=np.float32)
+            ground_truth[label_index] = 1.0
+            bottlenecks.append(bottleneck)
+            labels.append(ground_truth)
+    else:
+        # Fetch all bottlenecks
+        # Used for validation set and test set.
+        for label_index, label_name in enumerate(image_lists.keys()):
+            for image_index, image_name in enumerate(
+                    image_lists[label_name][category]):
+                bottleneck = get_bottleneck(
+                    sess, image_lists, label_name, image_index, image_dir, category,
+                    bottleneck_dir, model_name, jpeg_data_tensor, decoded_image_tensor,
+                    resized_input_tensor, bottleneck_tensor)
+                ground_truth = np.zeros(class_count, dtype=np.float32)
+                ground_truth[label_index] = 1.0
+                bottlenecks.append(bottleneck)
+                labels.append(ground_truth)
+    return bottlenecks, labels
--- code/nets/utils.py
+++ code/nets/utils.py
@@ -8,79 +8,84 @@ import os
 from glob import glob
 from six.moves import urllib
 
+
 # Return a sorted list of image files at image_dir
 def get_image_files(image_dir):
-  fs = glob("{}/*.jpg".format(image_dir))
-  fs = [os.path.basename(filename) for filename in fs]
-  return sorted(fs)
+    fs = glob("{}/*.jpg".format(image_dir))
+    fs = [os.path.basename(filename) for filename in fs]
+    return sorted(fs)
+
 
 # Download inception model if not already at 'inception_url'
 def download_inception_weights(inception_url, dest_dir):
-  if not os.path.exists(dest_dir):
-    os.makedirs(dest_dir)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
 
-  filename = inception_url.split("/")[-1]
-  filepath = os.path.join(dest_dir, filename)
-  if not os.path.exists(filepath):
-    def _progress(count, block_size, total_size):
-      sys.stdout.write("\r>> Downloading {} {:0.1f}".format(
-        filename, float(count * block_size) / float(total_size) * 100.0))
-      sys.stdout.flush()
-    filepath, _ = urllib.request.urlretrieve(inception_url, filepath, _progress)
-    print()
-    statinfo = os.stat(filepath)
-    tf.logging.info("Successfully downloaded", filename, statinfo.st_size,"bytes.")
-    tarfile.open(filepath, "r:gz").extractall(dest_dir)
+    filename = inception_url.split("/")[-1]
+    filepath = os.path.join(dest_dir, filename)
+    if not os.path.exists(filepath):
+        def _progress(count, block_size, total_size):
+            sys.stdout.write("\r>> Downloading {} {:0.1f}".format(
+                filename, float(count * block_size) / float(total_size) * 100.0))
+            sys.stdout.flush()
+
+        filepath, _ = urllib.request.urlretrieve(inception_url, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        tf.logging.info("Successfully downloaded", filename, statinfo.st_size, "bytes.")
+        tarfile.open(filepath, "r:gz").extractall(dest_dir)
+
 
 def create_image_lists(image_dir):
-  result = {}
+    result = {}
 
-  training_images = []
-  testing_images = []
-  validation_images = []
+    training_images = []
+    testing_images = []
+    validation_images = []
 
-  for category in ["train", "test", "val"]:
-    category_path = os.path.join(image_dir, category)
-    try:
-      class_labels = next(os.walk(category_path))[1]
-      class_labels.sort()
-    except StopIteration:
-      sys.exit("ERROR: Missing either train/test/val folders in image_dir")
+    for category in ["train", "test", "val"]:
+        category_path = os.path.join(image_dir, category)
+        try:
+            class_labels = next(os.walk(category_path))[1]
+            class_labels.sort()
+        except StopIteration:
+            sys.exit("ERROR: Missing either train/test/val folders in image_dir")
+        for cls in class_labels:
+            class_path = os.path.join(category_path, cls)
+            if category == "train":
+                training_images.append(get_image_files(class_path))
+            if category == "test":
+                testing_images.append(get_image_files(class_path))
+            if category == "val":
+                validation_images.append(get_image_files(class_path))
+
     for cls in class_labels:
-      class_path = os.path.join(category_path, cls)
-      if category == "train":
-        training_images.append(get_image_files(class_path))
-      if category == "test":
-        testing_images.append(get_image_files(class_path))
-      if category == "val":
-        validation_images.append(get_image_files(class_path))
+        result[cls] = {
+            "training": training_images[class_labels.index(cls)],
+            "testing": testing_images[class_labels.index(cls)],
+            "validation": validation_images[class_labels.index(cls)],
+        }
+    return result
 
-  for cls in class_labels:
-    result[cls] = {
-      "training": training_images[class_labels.index(cls)],
-      "testing": testing_images[class_labels.index(cls)],
-      "validation": validation_images[class_labels.index(cls)],
-    }
-  return result
 
 # Return a path to an image with the given label at the given index
 def get_image_path(image_lists, label_name, index, image_dir, category):
-  if label_name not in image_lists:
-    tf.logging.fatal("Label does not exist %s.", label_name)
-  label_lists = image_lists[label_name]
-  if category not in label_lists:
-    tf.logging.fatal("Category does not exist %s.", category)
-  category_list = label_lists[category]
-  if not category_list:
-    tf.logging.fatal("Label %s has no images in the category %s.", label_name, category)
-  mod_index = index % len(category_list)
-  base_name = category_list[mod_index]
+    if label_name not in image_lists:
+        tf.logging.fatal("Label does not exist %s.", label_name)
+    label_lists = image_lists[label_name]
+    if category not in label_lists:
+        tf.logging.fatal("Category does not exist %s.", category)
+    category_list = label_lists[category]
+    if not category_list:
+        tf.logging.fatal("Label %s has no images in the category %s.", label_name, category)
+    mod_index = index % len(category_list)
+    base_name = category_list[mod_index]
 
-  if("train" in category):
-    full_path = os.path.join(image_dir, "train", label_name, base_name)
-  elif("test" in category):
-    full_path = os.path.join(image_dir, "test", label_name, base_name)
-  elif("val" in category):
-    full_path = os.path.join(image_dir, "val", label_name, base_name)
+    if ("train" in category):
+        full_path = os.path.join(image_dir, "train", label_name, base_name)
+    elif ("test" in category):
+        full_path = os.path.join(image_dir, "test", label_name, base_name)
+    elif ("val" in category):
+        full_path = os.path.join(image_dir, "val", label_name, base_name)
 
-  return full_path
+    return full_path
